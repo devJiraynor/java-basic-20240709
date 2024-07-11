48bbe187ab6500faf19ab4b5be631b8c31a84e97
postforty <75767073+postforty@users.noreply.github.com>
2024-07-11 16:51:25 +0900

    2024.07.11. 16:50

--- basic1/src/chapter1/practice/Practice3.java
+++ basic1/src/chapter1/practice/Practice3.java
@@ -15,10 +15,30 @@ public class Practice3 {
 		
 		String statment = "내가 그린 기린 그림은 잘 그린 기린 그림이고 네가 그린 기린 그림은 잘 못그린 기린 그림이다.";
 		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("시작 인덱스 : ");
+		int startIndex = scanner.nextInt();
+		System.out.print("종료 인덱스 : ");
+		int endIndex = scanner.nextInt();
+		
+		scanner.close();
+		
+		char[] statementChars = statment.toCharArray();
+		char[] resultChars = new char[endIndex - startIndex];
+		
+		for (int index = 0; index < resultChars.length; index++) {
+			resultChars[index] = statementChars[startIndex + index];
+		}
+		
+		System.out.println(resultChars);
+		
 		
 	}
 
 }
+
+
+
 
 
 
