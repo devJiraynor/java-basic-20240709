ebc2db23776a31c793abd855c05248e4e649aa20
postforty <75767073+postforty@users.noreply.github.com>
2024-07-11 17:45:50 +0900

    2024.07.11. 17:50

--- basic1/src/chapter2/ClassAndObject.java
+++ basic1/src/chapter2/ClassAndObject.java
@@ -9,6 +9,7 @@ class ExampleClass1 {
 	double attribute2;
 	
 	// 기능 : 클래스가 가질수 있는 행동이나 작업 (메서드)
+	
 	// [접근제어자] 반환타입 메서드명 (매개변수타입 매개변수명[, ...])
 	// { 메서드 기능 구현 }
 	int method1 (int arg1) {
@@ -18,6 +19,62 @@ class ExampleClass1 {
 	}
 }
 
+// 스마트폰
+// =속성=
+// - 제조사 String
+// - 제조일자 String
+// - 모델명 String
+// - 전화번호 String
+// - 어플리스트 String[]
+// - 전원상태 boolean
+// =기능=
+// - 스마트폰정보보기
+// - 전화걸기
+// - 어플설치
+// - 문자보내기
+// - 전원누르기
+
+class SmartPhone {
+	
+	String manufacturer;
+	String manufactureDate;
+	String modelName;
+	String telNumber;
+	String[] appList = new String[0];
+	boolean powerStatus;
+	
+	void printInformation() {
+		System.out.println("===== 정보 =====");
+		System.out.println("제조사 : " + manufacturer);
+		System.out.println("제조날짜 : " + manufactureDate);
+		System.out.println("모델명 : " + modelName);
+		System.out.println("전화번호 : " + telNumber);
+	}
+	
+	void call(String toTelNumber) {
+		System.out.println(telNumber + "가 " + toTelNumber + "로 전화를 겁니다.");
+	}
+	
+	void appInstall(String appName) {
+		String[] newAppList = new String[appList.length + 1];
+		
+		for (int index = 0; index < appList.length; index++) {
+			newAppList[index] = appList[index];
+		}
+		newAppList[newAppList.length - 1] = appName;
+		appList = newAppList;
+	}
+	
+	void sendMessage(String toTelNumber, String message) {
+		System.out.println(telNumber + "가 " + toTelNumber + "에게 " + message + "를 전송했습니다.");
+	}
+	
+	void pressPowerButton() {
+		powerStatus = !powerStatus;
+	}
+	
+}
+
 public class ClassAndObject {
 
 	public static void main(String[] args) {
@@ -25,3 +82,6 @@ public class ClassAndObject {
 	}
 
 }
+
+
+
